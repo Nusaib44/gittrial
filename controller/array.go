140a18caad9642a124f4f6264b2fba2769528511
Nusaib <muhammednusaib44@gmail.com>
2023-03-28 14:03:39 +0530

    last summit

--- controller/array.go
+++ controller/array.go
@@ -1,5 +1,7 @@
 package controller
 
+import "fmt"
+
 func initarray() [20]int {
 	arr := [20]int{}
 	for i := 1; i <= 20; i++ {
@@ -8,7 +10,7 @@ func initarray() [20]int {
 	return arr
 }
 
-func Displayaray() [20]int {
+func Displayaray() {
 	array := initarray()
-	return array
+	fmt.Println(array)
 }
