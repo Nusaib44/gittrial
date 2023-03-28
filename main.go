42045e52d430fb809277658f1d474c76cef94963
Nusaib <muhammednusaib44@gmail.com>
2023-03-28 13:41:23 +0530

    second commit

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gitstudy/controller"
+)
 
 func main() {
 	fmt.Println("Hello")
+	print()
 
 }
 
 func print() {
 	println("hello this function is used to print the intput")
+	controller.Displayaray()
 }
